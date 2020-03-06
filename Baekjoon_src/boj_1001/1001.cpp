abf9e1ee822665b25c8ef5f91f9d7501cf00e339
WOOILAHN <boxer1532@naver.com>
2020-03-06 11:01:00 +0900

    Solve the problem 1001

---
+++ Baekjoon_src/boj_1001/1001.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+
+	int a, b;
+	scanf("%d %d", &a, &b);
+
+	printf("%d", a - b);
+
+	return 0;
+}
--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Baekjoon Online Judge Code
 
 ## Code List of Problem
 boj_1000: A+B
-
+boj_1001: A-B
