ad1f322ac8ccb2d550947cb7a795133b3ad74bca
WOOILAHN <boxer1532@naver.com>
2020-07-23 14:18:11 +0900

    Solve the problem 10953

---
+++ Baekjoon_src/boj_10953/10953.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+	int T, A, B;
+	scanf("%d", &T);
+
+	while (T--) {
+		scanf("%d,%d", &A, &B);
+		printf("%d\n", A + B);
+	}
+
+	return 0;
+}
