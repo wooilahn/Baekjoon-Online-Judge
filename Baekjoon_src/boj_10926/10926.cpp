b2df30aa4f08e35861bf645906a8c2581a90dd00
wooilahn <boxer1532@gmail.com>
2022-02-15 18:44:16 +0900

    Solve the problem 10926

---
+++ Baekjoon_src/boj_10926/10926.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+using namespace std;
+char str[55] = {0};
+
+int main(){
+  scanf("%s",str);
+  printf("%s", str);
+  printf("??!");
+
+  return 0;
+}
