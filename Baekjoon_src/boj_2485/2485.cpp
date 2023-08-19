c6443346e9793ece08d989a2134524527dd9f224
wooilahn <boxer1532@gmail.com>
2023-08-20 01:09:59 +0900

    Update 2485.cpp

--- Baekjoon_src/boj_2485/2485.cpp
+++ Baekjoon_src/boj_2485/2485.cpp
@@ -1,47 +1,45 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
-vector<int> arr;
-vector<int> dist;
 
-int gcd(int a, int b){
-  return b ? gcd(b, a%b) : a;
+vector<int> v;
+
+int gcd(int g1, int g2) {
+	while (g2 != 0) {
+		int tmp = g1 % g2;
+		g1 = g2;
+		g2 = tmp;
+	}
+
+	return g1;
 }
 
-int main(){
-  int N;
-  int min = __INT_MAX__;
-  scanf("%d", &N);
+int main() {
+	int N;
+	scanf("%d", &N);
 
-  for(int i=0; i<N; i++){
-    int num;
-    scanf("%d", &num);
-    arr.push_back(num);
-  }
+	for (int i = 0; i < N; i++) {
+		int num;
+		scanf("%d", &num);
+		v.push_back(num);
+	}
 
-  for(int i=1; i<N; i++){
-    dist.push_back(arr[i] - arr[i-1]);
-  }
+	int cg = gcd(v[1]-v[0], v[2]-v[1]);
 
-  for(int i=1; i<N-1; i++){
-    int temp = gcd(dist[i-1], dist[i]);
-    if(min > temp) min = temp;
-  }
+	for (int i = 3; i < N; i++) {
+		cg = gcd(cg, v[i]-v[i-1]);
+	}
 
-  int ans = 0;
+	int ans = 0;
 
-  for(int i=0; i<N; i++){
-    if(i+1 == N)
-      break;
-    if(arr[i+1] - arr[i] != min){
-      int tmp = (arr[i+1] - arr[i])/min - 1;
-      ans += tmp;
-    }
-  }
+	for (int i = 1; i < N; i++) {
+		int d = v[i] - v[i-1];
 
-  printf("%d\n", ans);
-  return 0;
+		ans += (d/cg) - 1;
+	}
+
+	printf("%d\n", ans);
+	return 0;
 }
